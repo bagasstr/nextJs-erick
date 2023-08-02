5af01132cc8ade8d4f49f419158a03be9a543e26
bagasstr <bagassatrio2805@gmail.com>
2023-08-03 04:09:19 +0700

    first

--- src/app/api/getdata/route.js
+++ src/app/api/getdata/route.js
@@ -2,5 +2,5 @@ import { NextResponse } from "next/server";
 import data from "./dataTerbaru.json";
 
 export async function GET(req) {
-  return NextResponse.json({ data });
+  return NextResponse.json(data);
 }
